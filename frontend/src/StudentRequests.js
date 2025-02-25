e69696eff17ea68325d0466345e7f77050237cbe
Anthony Edward <Anthony_ehab@hotmail.com>
2025-02-25 22:30:53 +0200

    Student Requests update
    Updated the form and right now this is the initial deisgn, mostly quite basic and content-bland until i can add more.

--- frontend/src/StudentRequests.js
+++ frontend/src/StudentRequests.js
@@ -46,7 +46,7 @@ function StudentRequests() {
                     >
                         Calendar
                     </a>
-                    
+
                 </nav>
                 <div className="mt-auto p-6">
                     <button className="w-full text-left py-2 px-4 text-white hover:bg-[#007ECA]/60 rounded-xl">
@@ -58,7 +58,41 @@ function StudentRequests() {
             {/* Settings */}
         <div className="flex flex-col bg-blue-100 w-full h-screen">
             <h1 className="mt-10 ml-4 text-4xl font-bold text-[#072D4A]/90">Requests</h1>
-            
+
+            {/* Content */}
+            <div className="mt-10 ml-4 mr-96 bg-sky-700 rounded-xl p-6 text-center">
+                <b className="text-5xl block">Requests Form</b>
+
+                <div className="mt-6">
+                    <b className="text-2xl">Type of Request:</b>
+                    <select
+                        name="TypeOfRequest"
+                        className="ml-3 font-bold text-black rounded-xl p-1 text-center"
+                    >
+                        <option>Mobility Assistance</option>
+                        <option>Medication storage</option>
+                        <option>Personal caregiver tag-along request</option>
+                        <option>Extra time on exam</option>
+                        <option>Assignment extension</option>
+                        <option>Note taker request</option>
+                        <option>Mental support</option>
+                        <option>Technology request</option>
+                    </select>
+                </div>
+
+                <div className="mt-6 text-left">
+                    <b className="text-2xl">Relevant details:</b>
+                    <textarea
+                        className="mt-2 block leading-normal rounded-xl font-bold p-3 w-full h-40 text-black"
+                        placeholder="Add your details here..."
+                    />
+                    <center>
+                        <input type="submit" className="m-2 bg-white text-black p-1 rounded-lg " value="submit" />
+                        <input type="reset" className="m-2 bg-white text-black p-1 rounded-lg " value="reset" />
+                    </center>
+    </div>
+    </div>
+
         </div>
         </div>
   );
