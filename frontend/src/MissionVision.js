3993c6e2ccaa96797d72a83feb4773152ea84fb3
Mohamed Kazan <Mohamed.Kazan24d@eslsca.edu.eg>
2025-03-01 16:19:39 +0200

    Updated MissionVision.js

---
+++ frontend/src/MissionVision.js
@@ -0,0 +1,28 @@
+import React from "react";
+import AccessabilityFeatures from './AccessibilityFeatures';
+
+function MissionVision() {
+  return ( 
+    <section>
+    <img src="EUL.png" alt="ESLSCA University Logo">
+</section>
+
+<h2>Our Mission & Vision</h2>
+
+<section>
+    <div>
+        <img src="Mission.JPG" alt="Mission Image">
+        <p>Providing tailored support, reasonable accommodations, and advocacy,
+         ensuring that students with disabilities have equitable access to all aspects of university life and a supportive learning experience.
+        </p>
+    </div>
+    <div>
+        <img src="Vision.jpg" alt="Vision Image">
+        <p>Fostering an inclusive and accessible educational environment where students of all abilities can fully participate,
+         excel, and achieve their academic potential.
+        </p>
+    </div>
+</section>
+);
+}
+export default MissionVision;
