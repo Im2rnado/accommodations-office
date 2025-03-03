5a6a770ce435680c7af5981f2cd257d7ee499044
Anthony Edward <Anthony_ehab@hotmail.com>
2025-03-03 21:14:45 +0200

    added active tickets section to SR
    Student Requests now has a pending requests div

--- frontend/src/StudentRequests.js
+++ frontend/src/StudentRequests.js
@@ -121,7 +121,24 @@ function StudentRequests() {
                 </button>
                 </form>
             </div>
-            <div className=" h-[100%] m-auto ml-0 w-[22%]  bg-[#007ECA] rounded-xl p-6 text-center"></div>
+            <div className=" h-[100%] m-auto ml-0 w-[22%]  bg-[#007ECA] rounded-xl p-6 text-center">
+                <b className="text-3xl block">Requests Form</b>
+                <div className="mt-3 rounded-xl h-max bg-[#072D4A]/10">
+                    Mobility assistance <br/>
+                    ID: F-2024-089 <br/>
+                    Pending: 2024-02-20 <br/>
+                </div>
+                <div className="mt-3 rounded-xl h-max bg-[#072D4A]/10">
+                    Extra time on exam <br/>
+                    ID: F-2024-067 <br/>
+                    Approved: 2024-01-15 <br/>
+                </div>
+                <div className="mt-3 rounded-xl h-max bg-[#072D4A]/10">
+                    Extra time on exam <br/>
+                    ID: F-2024-067 <br/>
+                    Unapproved: 2024-01-15 <br/>
+                </div>
+            </div>
           </div>
         </div>
         </div>
